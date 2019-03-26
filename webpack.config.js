7f9125e4c5caf5f1c054227accbafd14639fa687
Sessionking <rainlxyking@gmail.com>
2019-03-26 19:38:44 +0800

    格式化
    格式化

--- webpack.config.js
+++ webpack.config.js
@@ -7,33 +7,33 @@ module.exports = {
   mode: 'development',
   entry: {
     // another: './src/another-module.js',
-    index: './src/index.js'
+    index: './src/index.js',
   },
   output: {
     filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
     splitChunks: {
-      chunks: 'all'
-    }
+      chunks: 'all',
+    },
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist', //告诉webpack-dev-server 去./dist 找index.html 入口文件，webpack-dev-server --open 运行命令直接在浏览器中打开文件了
-    hot: true
+    contentBase: './dist', // 告诉webpack-dev-server 去./dist 找index.html 入口文件，webpack-dev-server --open 运行命令直接在浏览器中打开文件了
+    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({title: 'Output Management'}),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
